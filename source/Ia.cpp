b1226c03fb2cdcf802a46b62da91da45aed142c5
Tristan FLATOT <tristan.flatot@epitech.eu>
2018-05-31 12:21:27 +0200

    ia

--- source/Ia.cpp
+++ source/Ia.cpp
@@ -34,7 +34,7 @@ void    Ia::update()
 void	Ia:fill_dangerosity(int bombLen, int y, int x)
 {
 	for (int n = x; n < (x + bombLen); n++) {
-		if (n > 0 || map[y][n] == 16 || map[y][n] == 32)
+		if (n > 13 || map[y][n] == 16 || map[y][n] == 32)
 			break ;
 		d_map[y][n] = 2;
 	}
@@ -49,7 +49,7 @@ void	Ia:fill_dangerosity(int bombLen, int y, int x)
 		d_map[n][x] = 2;
 	}
 	for (n = y; n < (y + bombLen); n++) {
-		if (n > 0 || map[n][x] == 16 || map[n][x] == 32)
+		if (n > 11 || map[n][x] == 16 || map[n][x] == 32)
 			break ;
 		d_map[n][x] = 2;
 	}
@@ -69,15 +69,55 @@ void	Ia::scaleBomb()
 	}
 }
 
+void	find_best_way(int y, int x, int ite)
+{
+	if (distance_map[y][x] == ite) {
+		if (map[y][x + 1] != 16 && map[y][x + 1] != 32) {
+			_direction[y][x + 1] = direction[y][x];
+			distance[y][x + 1] = d + 1;
+		}
+		if (map[y][x + 1] != 16 && map[y][x + 1] != 32) {
+			direction[y][x - 1] = direction[y][x];
+			distance[y][x - 1]= d + 1;
+		}
+		if (map[y][x + 1] != 16 && map[y][x + 1] != 32) {
+			direction[y+1][x] = direction[y][x];
+			distance[y + 1][x]= d + 1;
+		}
+		if (map[y][x + 1] != 16 && map[y][x + 1] != 32) {
+			direction[y-1][x] = direction[y][x];
+			distance[y - 1][x]= d + 1;
+		}
+	}
+}
+
 void	Ia::defensive_mode(int ite)
 {
+	fill_distance_map(_y, _x);
 	for (int y = 0; y < 11; y++) {
 		for (int x = 0; x < 13; x++) {
-			if (d_map[y][x] == 0)
-				direction = map[y][x];
+			find_best_way(y, x, ite);
 		}
 	}
 	defensive_mode(ite + 1);
+}
+
+void	Ia::fill_distance_map(int n, int i)
+{
+	for (int y = 0; y < 11; y++) {
+		for (int x = 0; x < 13; x++) {
+			if (y == n && x == 1)
+				distance_map[y][x] = 0;
+			else
+				distance_map[y][x] = -1;
+			_direction[y][x] = -1;
+		}
+	}
+}
+
+void	Ia::active_mode()
+{
+
 }
 
 void    Ia::analysMap()
