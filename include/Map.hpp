d12b3302ac8869ce8626bf78fdc1fb7ea73fa272
Kevin Freire <kevin.freire@epitech.eu>
2018-06-04 12:50:34 +0200

    Save and load Mapvector

--- include/Map.hpp
+++ include/Map.hpp
@@ -8,16 +8,24 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
+#include <fstream>
 
 namespace bbm {
 	class Map {
 	public:
 		Map();
 		Map(std::vector<std::vector<int> > map);
+		void save();
+		void load();
 		void display();
+		std::vector<std::vector<int>> getMap() const{ return _map ;};
 	private:
+		std::vector<int> getVecFromLine(std::string line, std::string delimiter) const;
 		int _length;
 		int _width;
 		std::vector<std::vector<int> > _map;
 	};
+
 }
+std::ostream& operator<< (std::ostream& stream, bbm::Map const& map);
--- source/Map.cpp
+++ source/Map.cpp
@@ -5,7 +5,7 @@
 ** Map.cpp
 */
 
-#include <iostream>
+#include <sstream>
 #include "Map.hpp"
 
 bbm::Map::Map() :
@@ -22,6 +22,7 @@ bbm::Map::Map(std::vector<std::vector<int>> map) :
 	_map(map)
 {
 	std::cout << "Map constructed" << std::endl;
+
 }
 
 void bbm::Map::display() {
@@ -30,4 +31,57 @@ void bbm::Map::display() {
 			std::cout << _map[j][i];
 		std::cout << std::endl;
 	}
+}
+
+
+std::ostream& operator<< (std::ostream& stream, bbm::Map const& map) {
+	std::vector<std::vector<int>> _map = map.getMap();
+	for (int j = 0; j < _map.size(); j++) {
+		for (int i = 0; i < _map[0].size(); i++) {
+			stream << _map[j][i];
+			if (i != _map[j].size() - 1)
+				stream << ",";
+		}
+		stream << std::endl;
+	}
+	return stream;
+}
+
+void bbm::Map::save()
+{
+	std::ofstream myfile;
+	myfile.open("MapSave.txt");
+	if (myfile.is_open() == false)
+		return;
+	myfile << *this;
+	myfile.close();
+}
+
+void bbm::Map::load()
+{
+	std::ifstream infile("MapSave.txt");
+	std::string line;
+	std::string delimiter = ",";
+	std::vector<std::vector<int>> finalvec;
+
+	while (std::getline(infile, line))
+	{
+		finalvec.push_back(this->getVecFromLine(line, delimiter));
+	}
+	this->_map = finalvec;
+}
+
+std::vector<int> bbm::Map::getVecFromLine(std::string line, std::string delimiter) const {
+		std::vector<int> tmp;
+		size_t pos = 0;
+		std::string token;
+		std::string::size_type sz;
+
+		while ((pos = line.find(delimiter)) != std::string::npos) {
+			token = line.substr(0, pos);
+			tmp.push_back(std::stoi(token, &sz));
+			line.erase(0, pos + delimiter.length());
+		}
+		tmp.push_back(std::stoi(line, &sz));
+		return tmp;
 }
